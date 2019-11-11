6b8c6f8c635934b86e88984fa71d43b1c9eb677d
Helen Lee <helenlee@amm.cc>
2019-11-11 12:13:17 -0800

    5.5 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';   // enables routing functionality
+
+import { HeroesComponent } from './heroes/heroes.component';  // this is where the Router goes once the routes are configured
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+@NgModule({   // @NgModule initializes the router and starts it listening for browser location changes
+  imports: [RouterModule.forRoot(routes)],    // the forRoot function configures the AppRoutingModule
+  exports: [RouterModule]   // exporting makes it available throughout the application
 })
 export class AppRoutingModule { }
