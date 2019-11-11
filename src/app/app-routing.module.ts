75710baf37860075ae3be42123b3ce20807a2184
Helen Lee <helenlee@amm.cc>
2019-11-11 12:33:19 -0800

    end of step 5 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';   // enables routing functionality
 
 import { HeroesComponent } from './heroes/heroes.component';  // this is where the Router goes once the routes are configured
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({   // @NgModule initializes the router and starts it listening for browser location changes
