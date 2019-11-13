c70965d4d2727fc5c8c19e8e8366d370be1f4f7d
Helen Lee <hjools@users.noreply.github.com>
2019-11-13 14:05:21 -0800

    annotations to main.js

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const url = require("url");
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600 });
+  win = new BrowserWindow({
+    width: 800, height: 600
+  });
 
-  // loading dist folder from Angular
+  // loading dist folder from Angular instead of index.html
   win.loadURL(
     url.format({
       pathname: path.join(__dirname, `/dist/index.html`),
@@ -16,21 +18,27 @@ function createWindow() {
     })
   );
 
+  // Open the DevTools
+  win.webContents.openDevTools();
+
+  // Emitted when window closed, so dereference the window object.
   win.on("closed", () => {
     win = null;
   })
 }
 
+// called after Electron has finished initialization
 app.on("ready", createWindow);
 
-// ensure app is closed when window is closed (for macOS)
+// quit when all windows are closed
 app.on("window-all-closed", () => {
+  // quit for macOS
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// initialize the app's main window
+// initialize the app's main window if dock icon clicked
 app.on("activate", () => {
   if (win == null) {
     createWindow();
